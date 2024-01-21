7132a10c6327c042ccdabf2e79451a3ef1efb67d
alex <al3xback@gmail.com>
2024-01-21 12:22:06 +0700

    missed wrapping into promise

--- js/util.js
+++ js/util.js
@@ -20,199 +20,253 @@ const createElement = (tag, className, text, attributes) => {
 };
 
 const createHeaderEl = () => {
-	const headerEl = createElement('header');
+	return new Promise((resolve, reject) => {
+		if (typeof createElement === 'undefined') {
+			reject(new Error('createElement is not defined!'));
+		} else {
+			const headerEl = createElement('header');
 
-	const headerTitleEl = createElement(
-		'h1',
-		'sr-only',
-		'Single price grid component'
-	);
+			const headerTitleEl = createElement(
+				'h1',
+				'sr-only',
+				'Single price grid component'
+			);
 
-	headerEl.appendChild(headerTitleEl);
+			headerEl.appendChild(headerTitleEl);
 
-	return headerEl;
+			resolve(headerEl);
+		}
+	});
 };
 
 const createCardEl = () => {
-	const cardEl = createElement('article', 'card');
+	return new Promise((resolve, reject) => {
+		if (typeof createElement === 'undefined') {
+			reject(new Error('createElement is not defined!'));
+		} else if (typeof createElementAttribute === 'undefined') {
+			reject(new Error('createElementAttribute is not defined!'));
+		} else {
+			const cardEl = createElement('article', 'card');
 
-	/* section join community */
-	const sectionJoinCommunityEl = createElement(
-		'section',
-		'card__block card__block--join-community'
-	);
+			/* section join community */
+			const sectionJoinCommunityEl = createElement(
+				'section',
+				'card__block card__block--join-community'
+			);
 
-	const sectionJoinCommunityTitleEl = createElement(
-		'h2',
-		'card__title',
-		'Join our community'
-	);
+			const sectionJoinCommunityTitleEl = createElement(
+				'h2',
+				'card__title',
+				'Join our community'
+			);
 
-	const sectionJoinCommunitySubtitleEl = createElement(
-		'mark',
-		'card__mark',
-		'30-day, hassle-free money back guarantee'
-	);
+			const sectionJoinCommunitySubtitleEl = createElement(
+				'mark',
+				'card__mark',
+				'30-day, hassle-free money back guarantee'
+			);
 
-	const sectionJoinCommunityDescriptionEl = createElement(
-		'p',
-		'card__desc',
-		'Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.'
-	);
+			const sectionJoinCommunityDescriptionEl = createElement(
+				'p',
+				'card__desc',
+				'Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.'
+			);
 
-	sectionJoinCommunityEl.appendChild(sectionJoinCommunityTitleEl);
-	sectionJoinCommunityEl.appendChild(sectionJoinCommunitySubtitleEl);
-	sectionJoinCommunityEl.appendChild(sectionJoinCommunityDescriptionEl);
+			sectionJoinCommunityEl.appendChild(sectionJoinCommunityTitleEl);
+			sectionJoinCommunityEl.appendChild(sectionJoinCommunitySubtitleEl);
+			sectionJoinCommunityEl.appendChild(
+				sectionJoinCommunityDescriptionEl
+			);
 
-	/* section group */
-	const sectionGroupEl = createElement('div', 'card__group');
+			/* section group */
+			const sectionGroupEl = createElement('div', 'card__group');
 
-	/* section monthly subscription */
-	const sectionMonthlySubscriptionEl = createElement(
-		'section',
-		'card__block card__block--monthly-subsription'
-	);
+			/* section monthly subscription */
+			const sectionMonthlySubscriptionEl = createElement(
+				'section',
+				'card__block card__block--monthly-subsription'
+			);
 
-	const sectionMonthlySubscriptionTitleEl = createElement(
-		'h3',
-		'card__title',
-		'Monthly Subscription'
-	);
+			const sectionMonthlySubscriptionTitleEl = createElement(
+				'h3',
+				'card__title',
+				'Monthly Subscription'
+			);
 
-	const sectionMonthlySubscriptionPriceEl = createElement('p', 'card__price');
+			const sectionMonthlySubscriptionPriceEl = createElement(
+				'p',
+				'card__price'
+			);
 
-	const sectionMonthlySubscriptionPriceAmountEl = createElement(
-		'span',
-		'num',
-		'$29'
-	);
+			const sectionMonthlySubscriptionPriceAmountEl = createElement(
+				'span',
+				'num',
+				'$29'
+			);
 
-	const sectionMonthlySubscriptionPriceLabelEl = createElement(
-		'span',
-		'label',
-		'per month'
-	);
+			const sectionMonthlySubscriptionPriceLabelEl = createElement(
+				'span',
+				'label',
+				'per month'
+			);
 
-	sectionMonthlySubscriptionPriceEl.appendChild(
-		sectionMonthlySubscriptionPriceAmountEl
-	);
-	sectionMonthlySubscriptionPriceEl.appendChild(
-		sectionMonthlySubscriptionPriceLabelEl
-	);
+			sectionMonthlySubscriptionPriceEl.appendChild(
+				sectionMonthlySubscriptionPriceAmountEl
+			);
+			sectionMonthlySubscriptionPriceEl.appendChild(
+				sectionMonthlySubscriptionPriceLabelEl
+			);
 
-	const sectionMonthlySubscriptionDescriptionEl = createElement(
-		'p',
-		'card__desc',
-		'Full access for less than $1 a day'
-	);
+			const sectionMonthlySubscriptionDescriptionEl = createElement(
+				'p',
+				'card__desc',
+				'Full access for less than $1 a day'
+			);
 
-	const sectionMonthlySubscriptionLinkEl = createElement(
-		'a',
-		'btn btn--primary btn--full',
-		'Sign Up',
-		[createElementAttribute('href', '#')]
-	);
+			const sectionMonthlySubscriptionLinkEl = createElement(
+				'a',
+				'btn btn--primary btn--full',
+				'Sign Up',
+				[createElementAttribute('href', '#')]
+			);
 
-	sectionMonthlySubscriptionEl.appendChild(sectionMonthlySubscriptionTitleEl);
-	sectionMonthlySubscriptionEl.appendChild(sectionMonthlySubscriptionPriceEl);
-	sectionMonthlySubscriptionEl.appendChild(
-		sectionMonthlySubscriptionDescriptionEl
-	);
-	sectionMonthlySubscriptionEl.appendChild(sectionMonthlySubscriptionLinkEl);
+			sectionMonthlySubscriptionEl.appendChild(
+				sectionMonthlySubscriptionTitleEl
+			);
+			sectionMonthlySubscriptionEl.appendChild(
+				sectionMonthlySubscriptionPriceEl
+			);
+			sectionMonthlySubscriptionEl.appendChild(
+				sectionMonthlySubscriptionDescriptionEl
+			);
+			sectionMonthlySubscriptionEl.appendChild(
+				sectionMonthlySubscriptionLinkEl
+			);
 
-	/* section why us */
-	const sectionWhyUsEl = createElement(
-		'section',
-		'card__block card__block--why-us'
-	);
+			/* section why us */
+			const sectionWhyUsEl = createElement(
+				'section',
+				'card__block card__block--why-us'
+			);
 
-	const sectionWhyUsTitleEl = createElement('h3', 'card__title', 'Why Us');
+			const sectionWhyUsTitleEl = createElement(
+				'h3',
+				'card__title',
+				'Why Us'
+			);
 
-	const sectionWhyUsListEl = createElement('ul', 'card__list');
+			const sectionWhyUsListEl = createElement('ul', 'card__list');
 
-	const benefits = [
-		'Tutorials by industry experts',
-		'Peer & expert code review',
-		'Coding exercises',
-		'Access to our GitHub repos',
-		'Community forum',
-		'Flashcard decks',
-		'New videos every week',
-	];
+			const benefits = [
+				'Tutorials by industry experts',
+				'Peer & expert code review',
+				'Coding exercises',
+				'Access to our GitHub repos',
+				'Community forum',
+				'Flashcard decks',
+				'New videos every week',
+			];
 
-	for (const benefit of benefits) {
-		const sectionWhyUsListItemEl = createElement('li', null, benefit);
+			for (const benefit of benefits) {
+				const sectionWhyUsListItemEl = createElement(
+					'li',
+					null,
+					benefit
+				);
 
-		sectionWhyUsListEl.appendChild(sectionWhyUsListItemEl);
-	}
+				sectionWhyUsListEl.appendChild(sectionWhyUsListItemEl);
+			}
 
-	sectionWhyUsEl.appendChild(sectionWhyUsTitleEl);
-	sectionWhyUsEl.appendChild(sectionWhyUsListEl);
+			sectionWhyUsEl.appendChild(sectionWhyUsTitleEl);
+			sectionWhyUsEl.appendChild(sectionWhyUsListEl);
 
-	sectionGroupEl.appendChild(sectionMonthlySubscriptionEl);
-	sectionGroupEl.appendChild(sectionWhyUsEl);
+			sectionGroupEl.appendChild(sectionMonthlySubscriptionEl);
+			sectionGroupEl.appendChild(sectionWhyUsEl);
 
-	cardEl.appendChild(sectionJoinCommunityEl);
-	cardEl.appendChild(sectionGroupEl);
+			cardEl.appendChild(sectionJoinCommunityEl);
+			cardEl.appendChild(sectionGroupEl);
 
-	return cardEl;
+			resolve(cardEl);
+		}
+	});
 };
 
 const createMainEl = () => {
-	const mainEl = createElement('main');
+	return new Promise((resolve, reject) => {
+		if (typeof createElement === 'undefined') {
+			reject(new Error('createElement is not defined!'));
+		} else {
+			const mainEl = createElement('main');
 
-	const mainContainerEl = createElement('div', 'container');
+			const mainContainerEl = createElement('div', 'container');
 
-	const cardEl = createCardEl();
+			createCardEl()
+				.then((el) => {
+					mainContainerEl.appendChild(el);
 
-	mainContainerEl.appendChild(cardEl);
+					mainEl.appendChild(mainContainerEl);
 
-	mainEl.appendChild(mainContainerEl);
-
-	return mainEl;
+					resolve(mainEl);
+				})
+				.catch((err) => {
+					reject(err);
+				});
+		}
+	});
 };
 
 const createFooterEl = () => {
-	const footerEl = createElement('footer');
+	return new Promise((resolve, reject) => {
+		if (typeof createElement === 'undefined') {
+			reject(new Error('createElement is not defined!'));
+		} else if (typeof createElementAttribute === 'undefined') {
+			reject(new Error('createElementAttribute is not defined!'));
+		} else {
+			const footerEl = createElement('footer');
 
-	const footerContainerEl = createElement('div', 'container');
+			const footerContainerEl = createElement('div', 'container');
 
-	const footerTextEl = createElement('p', null, 'Challenge by ');
+			const footerTextEl = createElement('p', null, 'Challenge by ');
 
-	const footerTextLinkCreatorEl = createElement(
-		'a',
-		'btn btn--link',
-		'Frontend Mentor',
-		[
-			createElementAttribute(
-				'href',
-				'https://www.frontendmentor.io?ref=challenge'
-			),
-			createElementAttribute('rel', 'noopener'),
-			createElementAttribute('target', '_blank'),
-		]
-	);
+			const footerTextLinkCreatorEl = createElement(
+				'a',
+				'btn btn--link',
+				'Frontend Mentor',
+				[
+					createElementAttribute(
+						'href',
+						'https://www.frontendmentor.io?ref=challenge'
+					),
+					createElementAttribute('rel', 'noopener'),
+					createElementAttribute('target', '_blank'),
+				]
+			);
 
-	const footerTextLinkCoderEl = createElement(
-		'a',
-		'btn btn--link',
-		'al3xback',
-		[
-			createElementAttribute('href', 'https://github.com/al3xback'),
-			createElementAttribute('rel', 'noopener'),
-			createElementAttribute('target', '_blank'),
-		]
-	);
+			const footerTextLinkCoderEl = createElement(
+				'a',
+				'btn btn--link',
+				'al3xback',
+				[
+					createElementAttribute(
+						'href',
+						'https://github.com/al3xback'
+					),
+					createElementAttribute('rel', 'noopener'),
+					createElementAttribute('target', '_blank'),
+				]
+			);
 
-	footerTextEl.appendChild(footerTextLinkCreatorEl);
-	footerTextEl.append('. Coded by ');
-	footerTextEl.appendChild(footerTextLinkCoderEl);
+			footerTextEl.appendChild(footerTextLinkCreatorEl);
+			footerTextEl.append('. Coded by ');
+			footerTextEl.appendChild(footerTextLinkCoderEl);
 
-	footerContainerEl.appendChild(footerTextEl);
+			footerContainerEl.appendChild(footerTextEl);
 
-	footerEl.appendChild(footerContainerEl);
+			footerEl.appendChild(footerContainerEl);
 
-	return footerEl;
+			resolve(footerEl);
+		}
+	});
 };
 
 export { createHeaderEl, createMainEl, createFooterEl };
